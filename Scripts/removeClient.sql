a635f5bc3e05a875a9a8a706015005cbd8b747ee
Matthew Allwright <matt.a.4890@gmail.com>
2020-04-12 19:25:01 -0600

    Fix removeClient not removing dependant users

--- Scripts/removeClient.sql
+++ Scripts/removeClient.sql
@@ -1,6 +1,24 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `removeClient`(IN cid int)
 BEGIN
-	DELETE FROM dependant WHERE client_dependee = cid;
+	DECLARE done INT DEFAULT FALSE;
+    DECLARE uid INT;
+	DECLARE curs CURSOR FOR
+	SELECT dependant.userID
+	FROM dependant
+	WHERE dependant.client_dependee = cid;
+    
+	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+    OPEN curs;
+    delLoop: LOOP
+		FETCH curs INTO uid;
+        IF done THEN
+			LEAVE delLoop;
+		END IF;
+        DELETE FROM dependant WHERE userID = uid;
+		DELETE FROM user WHERE userID = uid;
+	END LOOP;
+    CLOSE curs;
+    
 	DELETE FROM rents WHERE clientID = cid;
 	DELETE FROM bill WHERE clientID = cid;
 	DELETE FROM request WHERE clientID = cid;
