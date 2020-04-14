c416c5e5a92775da2b045208b3c0720ab51e59b6
GregoryOrd <51534487+GregoryOrd@users.noreply.github.com>
2020-04-14 13:36:39 -0600

    authentication comments added

--- API/Helpers/Authentication.cs
+++ API/Helpers/Authentication.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 
 namespace CPSC471_RentalSystemAPI.Helpers
 {
+    //Enum containing values for the five types of users that interact with the system.
+    //Each user type has specific priveledges (endpoints that they are allowed to use)
+    //All the umbrella for all user type is USER
     public enum USER_TYPE
     {
         USER,
@@ -19,17 +22,27 @@ namespace CPSC471_RentalSystemAPI.Helpers
 
     public class Authentication
     {
+        //Function to check if a user has provided the right id and password to access privledges of a certain user type.
         public static Boolean checkAuthentication(int userID, String password, USER_TYPE userType)
         {
+            //Database model object to interact with the MySQL database.
             DatabaseModel dbModel = new DatabaseModel();
 
+            //Calculate the password hash from the inputted password
             String calculatedHash = calculatePasswordHash(password);
+            
+            //Create MySQLParameters for the user id and password hash
             MySqlParameter[] Parameters = new MySqlParameter[2];
             Parameters[0] = new MySqlParameter("@u_ID", userID);
             Parameters[1] = new MySqlParameter("@p_hash", calculatedHash);
 
+            //Surrounded by a try block. Exceptions will be thrown in the case of failed authentication.
             try
             {
+                //Execute a different stored procedure, based on the user type
+                //This allows for querying different tables of the DB based on the user type.
+                //If the stored procedure returns one row, the user passes authentication.
+                //If zero rows or more than one row are returned by the stored procedure, the user fails authentication
                 switch (userType)
                 {
                     case USER_TYPE.USER:
@@ -64,11 +77,16 @@ namespace CPSC471_RentalSystemAPI.Helpers
                 }
             }catch(Exception e)
             {
+                //If an exception is thrown, authentication fails
                 return false;
             }
+            //If there is no exception thrown, but the user is still not found in the correct table, authentication fails.
             return false;
         }
 
+        //This function takes a password and calculates the hash for that password.
+        //Currently, the hash is the same as the password. In a real world implementation, this
+        //would be changed to use a proper hash function for better password security.
         public static String calculatePasswordHash(String password)
         {
             return password;
